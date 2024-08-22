d243a07a3050867511a5888150cef7d77fcc99ad
Joao Marcos <joao.barroso@alu.ufc.br>
2024-08-22 10:24:55 -0300

    Initial commit

---
+++ src/main/java/com/joao/barroso/certificationsapi/CertificationsApiApplication.java
@@ -0,0 +1,13 @@
+package com.joao.barroso.certificationsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificationsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificationsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joao/barroso/certificationsapi/CertificationsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joao.barroso.certificationsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificationsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
